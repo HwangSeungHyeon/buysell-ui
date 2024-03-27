5b5625b3bd9a78d68ebfde8d2a585742b0950359
HwangSeungHyeon <xlddy02@gmail.com>
2024-03-27 19:10:23 +0900

    ✨Feat: 게시글 정렬 기준
    최신순으로 정렬
    조회수 높은 순으로 정렬

--- src/examples/navbars/NavbarDefault.vue
+++ src/examples/navbars/NavbarDefault.vue
@@ -1,10 +1,184 @@
+<template>
+  <nav class="navbar navbar-expand-lg top-0">
+    <div :class="getContainerClass()">
+      <RouterLink
+        class="navbar-brand d-none d-md-block"
+        :class="getBrandClass()"
+        :to="{ name: 'presentation' }"
+        rel="tooltip"
+        title="Designed and Coded by Creative Tim"
+        data-placement="bottom"
+      >
+        BuySell
+      </RouterLink>
+      <section>
+        <MaterialInput
+          class="input-group-dynamic mb-1 me-10"
+          icon="search"
+          type="text"
+          placeholder="Search"
+        />
+      </section>
+      <div class="d-flex align-items-center">
+        <button
+          class="navbar-toggler shadow-none ms-2"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navigation"
+          aria-controls="navigation"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon mt-2"
+            ><span class="navbar-toggler-bar bar1"></span>
+            <span class="navbar-toggler-bar bar2"></span>
+            <span class="navbar-toggler-bar bar3"></span>
+          </span>
+        </button>
+        <div
+          class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0"
+          id="navigation"
+        >
+          <ul class="navbar-nav navbar-nav-hover">
+            <li class="nav-item d-flex align-items-center me-2" v-if="isAuthenticated">
+              <router-link to="/orders">
+                <MaterialButton
+                    class="btn btn-sm mb-0"
+                    variant="gradient"
+                    color="success"
+                    fullWidth>
+                  상품등록
+                </MaterialButton>
+              </router-link>
+            </li>
+            <li class="nav-item d-flex align-items-center">
+              <button
+                  v-if="isAuthenticated"
+                  class="btn btn-sm mb-0"
+                  @click="logout"
+                  :class="action.color"
+              >
+                로그아웃
+              </button>
+              <router-link v-else :to="action.route">
+                <button class="btn btn-sm mb-0" :class="action.color">
+                  {{ action.label }}
+                </button>
+              </router-link>
+            </li>
+            <li class="nav-item dropdown dropdown-hover mx-2" v-if="isAuthenticated">
+              <a
+                  role="button"
+                  class="nav-link ps-2 d-flex cursor-pointer align-items-center"
+                  :class="getTextColor()"
+                  id="dropdownMenuPages"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+              >
+                <i
+                    class="material-icons opacity-6 me-2 text-md"
+                    :class="getTextColor()"
+                >
+                  dashboard</i
+                >
+
+                <img
+                    :src="getArrowColor()"
+                    alt="down-arrow"
+                    class="arrow ms-2 d-lg-block d-none"
+                />
+                <img
+                    :src="getArrowColor()"
+                    alt="down-arrow"
+                    class="arrow ms-1 d-lg-none d-block ms-auto"
+                />
+              </a>
+              <div
+                  class="dropdown-menu dropdown-menu-animation ms-n3 dropdown-md p-3 border-radius-xl mt-0 mt-lg-3"
+                  aria-labelledby="dropdownMenuPages"
+              >
+                <div class="row d-none d-lg-block">
+                  <div class="col-12 px-4 py-2">
+                    <div class="row">
+                      <div class="position-relative">
+                        <div
+                            class="dropdown-header text-dark font-weight-bolder d-flex align-items-center px-1"
+                        >
+                          개인
+                        </div>
+                        <RouterLink
+                            :to="{ name: 'about' }"
+                            class="dropdown-item border-radius-md"
+                        >
+                          <span>My Page</span>
+                        </RouterLink>
+                        <RouterLink
+                            :to="{ name: 'four-t-pay' }"
+                            class="dropdown-item border-radius-md"
+                        >
+                          <span>Four-T-Pay</span>
+                        </RouterLink>
+                        <RouterLink
+                            :to="{ name: 'author' }"
+                            class="dropdown-item border-radius-md"
+                        >
+                          <span>profile</span>
+                        </RouterLink>
+                        <RouterLink
+                            :to="{ name: 'wishlist' }"
+                            class="dropdown-item border-radius-md"
+                        >
+                          <span>wishlist</span>
+                        </RouterLink>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="d-lg-none">
+                  <div
+                      class="dropdown-header text-dark font-weight-bolder d-flex align-items-center px-0"
+                  >
+                    개인
+                  </div>
+                  <RouterLink
+                      :to="{ name: 'about' }"
+                      class="dropdown-item border-radius-md"
+                  >
+                    <span>My Page</span>
+                  </RouterLink>
+                  <RouterLink
+                      :to="{ name: 'four-t-pay' }"
+                      class="dropdown-item border-radius-md"
+                  >
+                    <span>Four-T-Pay</span>
+                  </RouterLink>
+                  <RouterLink
+                      :to="{ name: 'author' }"
+                      class="dropdown-item border-radius-md"
+                  >
+                    <span>profile</span>
+                  </RouterLink>
+                  <RouterLink
+                      :to="{ name: 'wishlist' }"
+                      class="dropdown-item border-radius-md"
+                  >
+                    <span>wishlist</span>
+                  </RouterLink>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
 <script setup>
 import { RouterLink } from "vue-router";
 import { ref, watch } from "vue";
 import { useWindowsWidth } from "../../assets/js/useWindowsWidth";
-//Vue Material Kit 2 components
 import MaterialInput from "@/components/MaterialInput.vue";
-// images
 import ArrDark from "@/assets/img/down-arrow-dark.svg";
 import DownArrWhite from "@/assets/img/down-arrow-white.svg";
 import MaterialButton from "@/components/MaterialButton.vue";
@@ -63,14 +237,14 @@ if (type.value === "mobile") {
   textDark.value = false;
 }
 watch(
-  () => type.value,
-  (newValue) => {
-    if (newValue === "mobile") {
-      textDark.value = true;
-    } else {
-      textDark.value = false;
+    () => type.value,
+    (newValue) => {
+      if (newValue === "mobile") {
+        textDark.value = true;
+      } else {
+        textDark.value = false;
+      }
     }
-  }
 );
 
 // set arrow  color
@@ -99,209 +273,13 @@ const getTextColor = () => {
 
 const getBrandClass = () => [
   (props.transparent && textDark.value) || !props.transparent
-    ? "text-dark font-weight-bolder ms-sm-3"
-    : "text-white font-weight-bolder ms-sm-3",
+      ? "text-dark font-weight-bolder ms-sm-3"
+      : "text-white font-weight-bolder ms-sm-3",
 ];
 
 const getContainerClass = () => [
   props.transparent || props.light || props.dark
-    ? "container"
-    : "container-fluid px-0",
+      ? "container"
+      : "container-fluid px-0",
 ];
 </script>
-<template>
-  <nav class="navbar navbar-expand-lg top-0">
-    <div :class="getContainerClass()">
-      <RouterLink
-        class="navbar-brand d-none d-md-block"
-        :class="getBrandClass()"
-        :to="{ name: 'presentation' }"
-        rel="tooltip"
-        title="Designed and Coded by Creative Tim"
-        data-placement="bottom"
-      >
-        BuySell
-      </RouterLink>
-      <section>
-        <MaterialInput
-          class="input-group-dynamic mb-1"
-          icon="search"
-          type="text"
-          placeholder="Search"
-        />
-      </section>
-      <div class="d-flex align-items-center">
-        <div
-          role="button"
-          class="nav-link ps-2 d-flex cursor-pointer align-items-end justify-content-end"
-          :class="getTextColor()"
-          id="dropdownMenuPages"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          `
-        </div>
-        <button
-          class="navbar-toggler shadow-none ms-2"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navigation"
-          aria-controls="navigation"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon mt-2"
-            ><span class="navbar-toggler-bar bar1"></span>
-            <span class="navbar-toggler-bar bar2"></span>
-            <span class="navbar-toggler-bar bar3"></span>
-          </span>
-        </button>
-        <div
-          class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0"
-          id="navigation"
-        >
-          <ul class="navbar-nav navbar-nav-hover ms-auto">
-            <router-link to="/orders">
-              <MaterialButton
-                class="my-4 mb-2"
-                variant="gradient"
-                color="success"
-                fullWidth
-              >
-                상품등록
-              </MaterialButton>
-            </router-link>
-            <li class="nav-item dropdown dropdown-hover mx-2">
-              <a
-                role="button"
-                class="nav-link ps-2 d-flex cursor-pointer align-items-center"
-                :class="getTextColor()"
-                id="dropdownMenuPages"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                <i
-                  class="material-icons opacity-6 me-2 text-md"
-                  :class="getTextColor()"
-                >
-                  dashboard</i
-                >
-
-                <img
-                  :src="getArrowColor()"
-                  alt="down-arrow"
-                  class="arrow ms-2 d-lg-block d-none"
-                />
-                <img
-                  :src="getArrowColor()"
-                  alt="down-arrow"
-                  class="arrow ms-1 d-lg-none d-block ms-auto"
-                />
-              </a>
-              <div
-                class="dropdown-menu dropdown-menu-animation ms-n3 dropdown-md p-3 border-radius-xl mt-0 mt-lg-3"
-                aria-labelledby="dropdownMenuPages"
-              >
-                <div class="row d-none d-lg-block">
-                  <div class="col-12 px-4 py-2">
-                    <div class="row">
-                      <div class="position-relative">
-                        <div
-                          class="dropdown-header text-dark font-weight-bolder d-flex align-items-center px-1"
-                        >
-                          개인
-                        </div>
-                        <RouterLink
-                          :to="{ name: 'about' }"
-                          class="dropdown-item border-radius-md"
-                        >
-                          <span>My Page</span>
-                        </RouterLink>
-                        <RouterLink
-                          :to="{ name: 'four-t-pay' }"
-                          class="dropdown-item border-radius-md"
-                        >
-                          <span>Four-T-Pay</span>
-                        </RouterLink>
-                        <RouterLink
-                          :to="{ name: 'author' }"
-                          class="dropdown-item border-radius-md"
-                        >
-                          <span>profile</span>
-                        </RouterLink>
-                        <RouterLink
-                          :to="{ name: 'wishlist' }"
-                          class="dropdown-item border-radius-md"
-                        >
-                          <span>wishlist</span>
-                        </RouterLink>
-<!--                        <RouterLink-->
-<!--                          :to="{ name: 'mysales' }"-->
-<!--                          class="dropdown-item border-radius-md"-->
-<!--                        >-->
-<!--                          <span>MySales</span>-->
-<!--                        </RouterLink>-->
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="d-lg-none">
-                  <div
-                    class="dropdown-header text-dark font-weight-bolder d-flex align-items-center px-0"
-                  >
-                    개인
-                  </div>
-                  <RouterLink
-                    :to="{ name: 'about' }"
-                    class="dropdown-item border-radius-md"
-                  >
-                    <span>My Page</span>
-                  </RouterLink>
-                  <RouterLink
-                    :to="{ name: 'four-t-pay' }"
-                    class="dropdown-item border-radius-md"
-                  >
-                    <span>Four-T-Pay</span>
-                  </RouterLink>
-                  <RouterLink
-                    :to="{ name: 'author' }"
-                    class="dropdown-item border-radius-md"
-                  >
-                    <span>profile</span>
-                  </RouterLink>
-                  <RouterLink
-                    :to="{ name: 'wishlist' }"
-                    class="dropdown-item border-radius-md"
-                  >
-                    <span>wishlist</span>
-                  </RouterLink>
-<!--                  <RouterLink-->
-<!--                    :to="{ name: 'mysales' }"-->
-<!--                    class="dropdown-item border-radius-md"-->
-<!--                  >-->
-<!--                    <span>MySales</span>-->
-<!--                  </RouterLink>-->
-                </div>
-              </div>
-            </li>
-            <li class="nav-item">
-              <button
-                v-if="isAuthenticated"
-                class="btn btn-sm mb-0"
-                @click="logout"
-                :class="action.color"
-              >
-                로그아웃
-              </button>
-              <router-link v-else :to="action.route">
-                <button class="btn btn-sm mb-0" :class="action.color">
-                  {{ action.label }}
-                </button>
-              </router-link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </nav>
-</template>
--- src/views/Presentation/PresentationView.vue
+++ src/views/Presentation/PresentationView.vue
@@ -1,76 +1,3 @@
-<script setup>
-import { onMounted, onUnmounted, ref } from "vue";
-import axios from "axios";
-import { useRouter } from "vue-router";
-
-//example components
-import NavbarDefault from "../..//examples/navbars/NavbarDefault.vue";
-import DefaultFooter from "../../examples/footers/FooterDefault.vue";
-import Header from "../../examples/Header.vue";
-
-// sections
-
-
-//images
-import vueMkHeader from "@/assets/img/vue-mk-header.jpg";
-
-const router = useRouter();
-const body = document.getElementsByTagName("body")[0];
-onMounted(() => {
-  body.classList.add("presentation-page");
-  body.classList.add("bg-gray-200");
-});
-onUnmounted(() => {
-  body.classList.remove("presentation-page");
-  body.classList.remove("bg-gray-200");
-});
-
-// nav-pill
-import setNavPills from "@/assets/js/nav-pills.js";
-onMounted(() => {
-  setNavPills();
-});
-const posts = ref([]);
-
-const fetchPosts = async () => {
-  try {
-    const response = await axios.get("/posts");
-    console.log("response", response);
-    posts.value = response.data.content;
-  } catch (error) {
-    console.error("게시물을 불러오는데 실패했습니다:", error);
-  }
-};
-
-onMounted(fetchPosts);
-
-const navigateToDetail = (postId) => {
-  router.push({ name: "posts", params: { postId } });
-};
-
-const formatDate = (dateString) => {
-  const date = new Date(dateString);
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-
-  return `${year}년 ${month}월 ${day}일 ${hours}시 ${minutes}분`;
-};
-const increaseViewCount = async (postId) => {
-  try {
-    await axios.post(`/posts/${postId}/views`);
-  } catch (error) {
-    console.error("조회수를 증가하는데 실패했습니다:", error);
-  }
-};
-const handlePostClick = async (postId) => {
-  await increaseViewCount(postId);
-  navigateToDetail(postId);
-};
-</script>
-
 <template>
   <div class="container position-sticky z-index-sticky top-0">
     <div class="row">
@@ -109,11 +36,13 @@ const handlePostClick = async (postId) => {
             <li class="nav-item">
               <a
                 class="nav-link mb-0 px-0 py-1 active"
+                :class="{ active: selectedSort === 'updatedAt,desc' }"
                 data-bs-toggle="tab"
-                href="#profile-tabs-simple"
+                href="#"
                 role="tab"
                 aria-controls="profile"
                 aria-selected="true"
+                @click.prevent="changeSort('updatedAt,desc')"
               >
                 최신 순
               </a>
@@ -121,11 +50,13 @@ const handlePostClick = async (postId) => {
             <li class="nav-item">
               <a
                 class="nav-link mb-0 px-0 py-1"
+                :class="{ active: selectedSort === 'view,desc' }"
                 data-bs-toggle="tab"
-                href="#dashboard-tabs-simple"
+                href="#"
                 role="tab"
                 aria-controls="dashboard"
                 aria-selected="false"
+                @click.prevent="changeSort('view,desc')"
               >
                 많이 본 순
               </a>
@@ -148,7 +79,7 @@ const handlePostClick = async (postId) => {
             <div class="card-body">
               <h5 class="card-title">{{ post.title }}</h5>
               <p class="card-text">작성자: {{ post.createdName }}</p>
-              <p class="card-text">작성일: {{ formatDate(post.createdAt) }}</p>
+              <p class="card-text">작성일: {{ formatDate(post.updatedAt) }}</p>
               <p class="card-text">조회수: {{ post.view }}</p>
             </div>
           </div>
@@ -156,6 +87,74 @@ const handlePostClick = async (postId) => {
       </div>
     </div>
   </div>
-
   <DefaultFooter />
 </template>
+
+<script setup>
+import { onMounted, onUnmounted, ref } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+import NavbarDefault from "../..//examples/navbars/NavbarDefault.vue";
+import DefaultFooter from "../../examples/footers/FooterDefault.vue";
+import Header from "../../examples/Header.vue";
+import vueMkHeader from "@/assets/img/vue-mk-header.jpg";
+import setNavPills from "@/assets/js/nav-pills.js";
+
+const router = useRouter();
+const body = document.getElementsByTagName("body")[0];
+
+onMounted(() => {
+  body.classList.add("presentation-page");
+  body.classList.add("bg-gray-200");
+  setNavPills();
+});
+
+onUnmounted(() => {
+  body.classList.remove("presentation-page");
+  body.classList.remove("bg-gray-200");
+});
+
+
+const selectedSort = ref('createdAt,desc');
+const posts = ref([]);
+
+//정렬 기준 변경 메서드
+const changeSort = (sortCriteria) => {
+  selectedSort.value = sortCriteria;
+  fetchPosts();
+};
+
+const fetchPosts = async () => {
+  try {
+    const response = await axios.get('/posts', {
+      params: {
+        sort: selectedSort.value,
+      },
+    });
+    posts.value = response.data.content;
+    console.log(posts.value)
+  } catch (error) {
+    console.error("게시물을 불러오는데 실패했습니다:", error);
+  }
+};
+
+onMounted(fetchPosts);
+
+const navigateToDetail = (postId) => {
+  router.push({ name: "posts", params: { postId } });
+};
+
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return `${year}년 ${month}월 ${day}일 ${hours}시 ${minutes}분`;
+};
+const handlePostClick = async (postId) => {
+  navigateToDetail(postId);
+};
+</script>
